04fde1ecc039569d6c622595b3827f03cbfeebbe
mickhakk <gakkmike@gmail.com>
2024-03-10 22:00:52 +0200

    logic and layout for password control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
-import {FormControl} from "@angular/forms";
+import {FormControl, Validators} from "@angular/forms";
+import {debounceTime, distinctUntilChanged} from "rxjs";
+
+export enum EPasswordStrengthStatuses {
+  NONE ,
+  EASY,
+  MEDIUM,
+  STRONG
+}
+
+const PASSWORD_STRENGTH_BY_INDEX = [
+  EPasswordStrengthStatuses.NONE,
+  EPasswordStrengthStatuses.EASY,
+  EPasswordStrengthStatuses.MEDIUM,
+  EPasswordStrengthStatuses.STRONG,
+];
+
+const ONLY_LETTERS_REGEX = /[a-zA-Z]/;
+const ONLY_DIGITS_REGEX = /\d/;
+const ONLY_SYMBOLS_REGEX = /[^a-zA-Z0-9]/;
 
 @Component({
   selector: 'app-root',
@@ -7,5 +26,38 @@ import {FormControl} from "@angular/forms";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  passwordFormControl = new FormControl('password');
+  passwordFormControl = new FormControl('', Validators.minLength(8));
+  passwordStrengthStatus = EPasswordStrengthStatuses.NONE;
+  passwordStrengthStatuses = EPasswordStrengthStatuses;
+
+  constructor() {
+    this.passwordFormControl.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      )
+      .subscribe((password) => {
+        if (!password || this.isPasswordInvalid) {
+          this.passwordStrengthStatus = EPasswordStrengthStatuses.NONE;
+          return;
+        }
+
+        let strengthStatusCounter = 0;
+        if (ONLY_LETTERS_REGEX.test(password)) {
+          strengthStatusCounter++;
+        }
+        if (ONLY_DIGITS_REGEX.test(password)) {
+          strengthStatusCounter++;
+        }
+        if (ONLY_SYMBOLS_REGEX.test(password)) {
+          strengthStatusCounter++;
+        }
+
+        this.passwordStrengthStatus = PASSWORD_STRENGTH_BY_INDEX[strengthStatusCounter];
+    });
+  }
+
+  get isPasswordInvalid(): boolean {
+    return this.passwordFormControl.invalid;
+  }
 }
